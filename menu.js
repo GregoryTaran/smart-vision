c1a3725a6dbe2cc6cfa07d0af80761584cf92bb6
GregoryTaran <4401006@gmail.com>
2025-10-16 17:36:48 +0100

    1

--- menu.js
+++
@@ -1,9 +0,0 @@
-
-function toggleMenu() {
-  const menu = document.getElementById('sideMenu');
-  menu.classList.toggle('open');
-}
-
-function toggleActive(el) {
-  el.classList.toggle('active');
-}
